c95267413b32c8784de3aca17ac02fe476f77b8d
‘gcd’ <it_gcd@163.com>
2021-09-28 22:20:16 +0800

    小方块

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ let imgList = document.getElementById("imgList");
 let imgArr = document.getElementsByTagName("img");
 imgList.style.width = 264 * imgArr.length + "px";
 
+let navDiv = document.getElementById("navDiv");
+let outer = document.getElementById("outer");
+navDiv.style.left = (outer.offsetWidth - navDiv.offsetWidth)/2 + "px";
+
+//设置默认图片索引
+let allA = document.getElementsByTagName("a");
+let index = 0;
+allA[index].style.backgroundColor="black";
