2c285d6eb663a257b9d3715f3414cb52b4c7cfc3
‘gcd’ <it_gcd@163.com>
2021-10-07 07:01:08 +0800

    点击按钮切换图片

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let imgList = document.getElementById("imgList");
 let imgArr = document.getElementsByTagName("img");
-imgList.style.width = 264 * imgArr.length + "px";
+imgList.style.width = 284 * imgArr.length + "px";
+//console.log(imgList.style.width)
 
 let navDiv = document.getElementById("navDiv");
 let outer = document.getElementById("outer");
@@ -10,3 +11,23 @@ navDiv.style.left = (outer.offsetWidth - navDiv.offsetWidth)/2 + "px";
 let allA = document.getElementsByTagName("a");
 let index = 0;
 allA[index].style.backgroundColor="black";
+
+console.log(allA[index].style.backgroundColor);
+
+for(let i = 0;i<=allA.length-1;i++){
+    //console.log(allA[i])
+    allA[i].addEventListener('click',function(){
+            imgList.style.left = -284 * i +"px";
+            index=i;
+            console.log(allA[index])
+            //allA[index].style.backgroundColor="black";
+            setA()
+        },false)
+}
+function setA(){
+    for(let i = 0;i<=allA.length-1;i++){
+        //console.log(allA[i])
+        allA[i].style.backgroundColor = '';
+    }
+    allA[index].style.backgroundColor="black";
+}
